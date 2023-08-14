b6e354e8f1d0b3afbfa9f670ba901157a5bdedf0
Alfredo Sánchez <alfred3ks@gmail.com>
2023-08-14 17:18:54 +0200

    Agrego evento onChange al input del NavBar.

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react';
+
 // Creamos el componente del Navbar:
 const NavBar = () => {
+  // defino estado para el input:
+  const [search, getSearch] = useState('');
+
+  console.log(search); // Aqui vemos el valor que introduce el usuario en el input
+
+  // Creamos nuestra funcion handle del input:
+  const handleInputChange = (e) => {
+    // Obtenemos los que se escribe dentro del input:
+    console.log(e.target.value);
+    getSearch(e.target.value);
+  };
+
   return (
     <div>
       <label htmlFor="event">Mi ticket</label>
-      <input type="text" placeholder="Busca tu evento favorito" id="event" />
+      <input
+        type="text"
+        placeholder="Busca tu evento favorito"
+        id="event"
+        onChange={handleInputChange}
+        value={search}
+      />
     </div>
   );
 };
 
 export default NavBar;
+
+/*
+NOTA:
+Como vemos el imput del buscador lleva dos propiedades que siempre van juntas que son onChange={handleInputChange} que recibe la funcion handle, la manejador, y value={search}, ambos atributos van siempre juntos.
+
+Tambien hemos declarado un estado, para poder obtener el valor del input debemos declarar estado, con useState().
+
+*/
